c49ce77de99bfe5c003f6924e9bfac454d5a9886
r-r-7 <rahulradhesha@gmail.com>
2023-11-09 15:05:34 +0530

    tti.py file added

---
+++ tti.py
@@ -0,0 +1,48 @@
+# import modules
+import cv2
+import pytesseract
+pytesseract.pytesseract.tesseract_cmd = "C:/Program Files/Tesseract-OCR/tesseract.exe"
+
+# read image
+img = cv2.imread("C:/Users/rahul/Desktop/images.jpg")
+
+# set configurations
+config = ('-l eng --oem 1 --psm 3')
+
+# Convert the image to gray scale 
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) 
+  
+# OTSU threshold performing
+ret, threshimg = cv2.threshold(gray, 0, 255, cv2.THRESH_OTSU | cv2.THRESH_BINARY_INV) 
+  
+# Specifying kernel size and structure shape.  
+rect_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (18, 18)) 
+  
+# Applying dilation on the threshold image 
+dilation = cv2.dilate(threshimg, rect_kernel, iterations = 1) 
+  
+# getting contours 
+img_contours, hierarchy = cv2.findContours(dilation, cv2.RETR_EXTERNAL,  cv2.CHAIN_APPROX_NONE) 
+  
+# Loop over contours and crop and extract the text file
+for cnt in img_contours: 
+    x, y, w, h = cv2.boundingRect(cnt) 
+      
+    # Drawing a rectangle
+    rect = cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2) 
+      
+    # Cropping the text block  
+    cropped_img = img[y:y + h, x:x + w] 
+      
+    # Open the text file in append mode 
+    file = open("recognized.txt", "a") 
+      
+    # Applying tesseract OCR on the cropped image 
+    text = pytesseract.image_to_string(cropped_img) 
+      
+    # Appending the text into file 
+    file.write(text) 
+    file.write("\n") 
+      
+    # Close the file 
+    file.close 
---
+++ tts.py
@@ -0,0 +1,16 @@
+# import Google text-to-speech
+from gtts import gTTS 
+import os
+
+# open text file
+file = open("C:/Users/rahul/recognized.txt", "r").read().replace("\n", " ")
+
+# set language as english
+language = "en"
+
+# convert text to speech
+speech = gTTS(text = str(file), lang = language, slow = False)
+
+#save speech in a mp3 file
+speech.save("voice.mp3")
+os.system("start voice.mp3")
